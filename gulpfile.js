5111c0c1b4d79e18649d216906d52e27b5de893d
ikrong <lun.i@outlook.com>
2020-04-15 23:15:15 +0800

    md注释

--- gulpfile.js
+++ gulpfile.js
@@ -32,9 +32,9 @@ function genProjectMD() {
     let mdStr = table.map(row => `| ${row.join(' | ')} |`).join('\n');
 
     let readme = fs.readFileSync('./README.md').toString()
-    readme = readme.split(/[\r\n]\[\/\/\]: # \(实现的内容表格\)[\r\n]/)
+    readme = readme.split(/[\r\n]\[\/\/\]: # \(实现的内容表格\)[\r\n]{2}/)
     readme[1] = mdStr
-    readme = readme.join('\n\[\/\/\]: # \(实现的内容表格\)\n')
+    readme = readme.join('\n\[\/\/\]: # \(实现的内容表格\)\n\n')
 
     fs.writeFileSync('./README.md', readme)
 }
