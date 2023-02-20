30c85f22ea149d530d34688b7316dc59310f5990
ikrong <lun.i@outlook.com>
2023-02-20 21:56:32 +0800

    add 双色球

--- src/lottery-ssq/index.js
+++ src/lottery-ssq/index.js
@@ -35,6 +35,18 @@ class SsqLottery {
 
     reward = -1
 
+    get rewardText() {
+        return {
+            0: '未中奖',
+            1: '一等奖',
+            2: '二等奖',
+            3: '三等奖',
+            4: '四等奖',
+            5: '五等奖',
+            6: '六等奖',
+        }[this.reward]
+    }
+
     el
     constructor(
         reds,
@@ -83,7 +95,7 @@ class SsqLottery {
                 matchRed++;
             }
         }
-        if (blueBall === lottery.blue) {
+        if (this.blue === lottery.blue) {
             matchBlue = true;
         }
         if (matchRed === 6 && matchBlue) {
@@ -104,14 +116,14 @@ class SsqLottery {
     }
 
     render() {
-        if(!this.el) {
+        if (!this.el) {
             const div = document.createElement('div')
             div.classList.add('lottery-info')
         }
 
         this.el.innerHTML = `<div class="lottery-code">${this.code}</div>
         <div class="lottery-number">
-            ${this.reds.map(num=>`<div class="red-ball">${num}</div>`)}
+            ${this.reds.map(num => `<div class="red-ball">${num}</div>`).join('')}
             <div class="blue-ball">${this.blue}</div>
         </div>
         <div class="lottery-reward">${this.reward}</div>
@@ -137,9 +149,11 @@ class LotterySelector {
     }
 
     init() {
-        const root = document.createElement('div')
+        const root = document.createDocumentFragment()
         const redBallContainer = document.createElement('div')
+        redBallContainer.classList.add('red-balls')
         const blueBallContainer = document.createElement('div')
+        blueBallContainer.classList.add('blue-balls')
         root.append(redBallContainer)
         root.append(blueBallContainer)
         this.allReds = Array(33).fill().map((_, i) => i + 1).map(num => {
@@ -163,44 +177,50 @@ class LotterySelector {
             }
         })
         this.el = root
-        root.addEventListener('click', this.clickListener = (e) => {
-            const target = e.target
-            const info = e.target.getAttribute('lottery')
-            if (info) {
-                const [type, num] = info.split('_')
-                if (!target.classList.contains('active')) {
-                    if (type === 'r' && this.reds.length < 6) {
-                        target.classList.add('active')
-                        this.reds.push(num)
-                    } else if (type === 'b') {
-                        this.allBlues.map(({ el }) => {
-                            if (el !== target) {
-                                el.classList.remove('active')
-                            }
-                        })
-                        target.classList.add('active')
-                        this.blue = num
-                    }
-                } else {
-                    target.classList.remove('active')
-                    if (type === 'r') {
-                        this.reds.splice(this.reds.indexOf(num), 1)
-                    } else if (type === 'b') {
-                        this.blue = ''
-                    }
-                }
-            }
+        redBallContainer.addEventListener('click', this.click)
+        blueBallContainer.addEventListener('click', this.click)
+        const codeInput = document.querySelector('.lottery-code')
+        codeInput.addEventListener('change', () => {
+            this.code = codeInput.value.trim()
         })
     }
 
+    click = (e) => {
+        const target = e.target
+        const info = e.target.getAttribute('lottery')
+        if (info) {
+            const [type, num] = info.split('_')
+            if (!target.classList.contains('active')) {
+                if (type === 'r' && this.reds.length < 6) {
+                    target.classList.add('active')
+                    this.reds.push(num)
+                } else if (type === 'b') {
+                    this.allBlues.map(({ el }) => {
+                        if (el !== target) {
+                            el.classList.remove('active')
+                        }
+                    })
+                    target.classList.add('active')
+                    this.blue = num
+                }
+            } else {
+                target.classList.remove('active')
+                if (type === 'r') {
+                    this.reds.splice(this.reds.indexOf(num), 1)
+                } else if (type === 'b') {
+                    this.blue = ''
+                }
+            }
+        }
+    }
+
     getSelect() {
-        if (this.reds.length === 6 && this.blue) {
+        if (this.reds.length === 6 && this.blue && this.code) {
             return new SsqLottery(this.reds, this.blue, this.code)
         }
     }
 
     destory() {
-        this.el.removeEventListener('click', this.clickListener)
         this.allReds = []
         this.allBlues = []
     }
@@ -232,6 +252,16 @@ class LotteryController {
         // </div>
         const latestRoot = document.querySelector('.lottery-container .lottery-latest')
         const selector = document.querySelector('.lottery-container .lottery-select .lottery-view .lottery-selectarea')
+        const btn = document.querySelector('.lottery-container .lottery-select .lottery-view .lottery-btn .lottery-check')
+
+        btn.addEventListener('click', () => {
+            const lottery = this.selector.getSelect()
+            lottery && lottery.checkReward().then(() => {
+                if (lottery.reward !== -1) {
+                    alert(lottery.rewardText)
+                }
+            })
+        })
 
         this.selector = new LotterySelector()
         selector.append(this.selector.el)
@@ -239,7 +269,7 @@ class LotteryController {
 
         const resp = await (await fetch('https://api-next.ikrong.com/ex/lottery/list/latest')).json()
         this.latest = resp.data.map(item => {
-            const reds = item.num.split(',')
+            const reds = item.nums.split(',')
             const blue = reds.pop()
             LotteryStore.set(item.code, { reds, blue })
 
@@ -247,7 +277,7 @@ class LotteryController {
             div.classList.add('lottery-info')
             div.innerHTML = `<div class="lottery-code">${item.code}</div>
             <div class="lottery-number">
-                ${reds.map(num=>`<div class="red-ball">${num}</div>`)}
+                ${reds.map(num => `<div class="red-ball">${num}</div>`).join('')}
                 <div class="blue-ball">${blue}</div>
             </div>
             `
